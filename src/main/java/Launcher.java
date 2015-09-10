3678ede12d510c7c6d30c36d0f77f76a9a834d2d
Kevin Randrup <kevinrandrup@gmail.com>
2015-09-10 15:21:35 -0400

    Created Launcher to start the application
    The Launcher will be the main class which starts the application.
    It currently shows the PlayerConfigurationScreen for testing but that
    will change in the future.

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,27 @@
+package main.java;
+
+import controllers.PlayerConfigurationController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+/**
+ * The Launcher is the starting point for our JavaFX Application.
+ * @author Kevin Randrup
+ */
+public class Launcher extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		//Example configuration. This will be changed later.
+		PlayerConfigurationController controller = new PlayerConfigurationController(1);
+		Scene scene = controller.getScene();
+		primaryStage.setScene(scene);
+		primaryStage.show();		
+	}	
+}
