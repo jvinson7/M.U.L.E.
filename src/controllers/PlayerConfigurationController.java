b1254fceea0b4ea41ea4fd857059511c76f928f2
Kevin Randrup <kevinrandrup@gmail.com>
2015-09-10 19:00:49 -0400

    Example for using @FXML to connect methods and UI elements (using PlayerConfigurationController).

--- src/controllers/PlayerConfigurationController.java
+++ src/controllers/PlayerConfigurationController.java
@@ -1,9 +1,16 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 /**
  * The PlayerConfigurationScreen allows a number of players to be configured with a name, race and color.
  * @author Kevin Randrup
- *
  */
 public class PlayerConfigurationController extends Controller {
 	
@@ -11,6 +18,19 @@ public class PlayerConfigurationController extends Controller {
 		
 	}
 	
+	@FXML
+	private Button doneButton;
+	
+	@FXML
+	private TextField nameTextField;
+	
+	@FXML
+	private ColorPicker colorPicker;
+	
+	@FXML
+	private ComboBox/*<Race>*/ raceComboBox;
+	
+			
 	@Override
 	protected String getSceneResourceName() {
 		return "resources/PlayerConfigurationScreen.fxml";
@@ -19,5 +39,16 @@ public class PlayerConfigurationController extends Controller {
 	@FXML
 	public void initialize() {
 		System.out.println("Initialize called");
+				
+		System.out.println("Done button: " + doneButton);
+		System.out.println("Name: " + nameTextField);
+		System.out.println("Color picker: " + colorPicker);
+		System.out.println("Race: " + raceComboBox);
 	}
+	
+	@FXML
+	private void done(Event event) {
+		System.out.println("Done pressed. Event: " + event);
+	}
+
 }
