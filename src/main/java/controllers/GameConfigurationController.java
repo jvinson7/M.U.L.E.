c997260acbc0ae691bb77afce4a43808325e09d9
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-09-11 16:46:43 -0400

    Tie model to view.

--- src/main/java/controllers/GameConfigurationController.java
+++ src/main/java/controllers/GameConfigurationController.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import com.sun.javafx.collections.ImmutableObservableList;
+import javafx.beans.binding.BooleanBinding;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -32,5 +35,25 @@ public class GameConfigurationController extends Controller {
   public void initialize() {
     // Populate difficulty box.
     difficultySelector.getItems().setAll(Difficulty.allDifficulties);
+
+    // Populate number of players box.
+    numberOfPlayersSelector.getItems()
+      .setAll(new ImmutableObservableList<Integer>(1, 2, 3, 4));
+  
+    // Enable done button when a difficulty and the number of players have been
+    // selected.
+    BooleanBinding gameConfigured = gameConfig.isConfigured;
+    doneButton.disableProperty().bind(gameConfigured.not());
+
+    // Bind UI to GameConfiguration model.
+    gameConfig.numberOfPlayersProperty()
+      .bindBidirectional(numberOfPlayersSelector.valueProperty());
+    gameConfig.difficultyProperty()
+      .bindBidirectional(difficultySelector.valueProperty());
+  }
+
+  @FXML
+  private void done(Event event) {
+    System.out.println("Done pressed. Event: " + event);
   }
 }
